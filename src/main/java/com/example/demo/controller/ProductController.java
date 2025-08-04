55451338b11788b45a078034664d40971ebc1ce7
dbstj0403 <wcn09088@naver.com>
2025-08-04 15:29:27 +0900

    init: project setting

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.Product;
+import com.example.demo.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+}
